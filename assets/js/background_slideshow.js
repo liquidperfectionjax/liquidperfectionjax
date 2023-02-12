b2d48fef543749ee0b5c74a67c10e11de42b5d89
Cygnus69 <hilton992003@yahoo.com>
2023-02-12 12:03:29 -0800

    Update background_slideshow.js
    Signed-off-by: Cygnus69 <hilton992003@yahoo.com>

--- assets/js/background_slideshow.js
+++ assets/js/background_slideshow.js
@@ -2,11 +2,27 @@ var i = 0;
 var images = [];
 var slideTime = 3000; // 3 seconds
 
-images[0] = 'assets/images/work/ash.webp';
-images[1] = 'assets/images/work/ash1.webp';
-images[2] = 'assets/images/work/ash2.webp';
-images[3] = 'assets/images/work/ash3-300x225.webp';
-images[4] = 'assets/images/work/ash4.webp';
+images[0] = 'assets/images/work/Liquidperfection 000.jpg';
+images[1] = 'assets/images/work/Liquidperfection 001.jpg';
+images[2] = 'assets/images/work/Liquidperfection 002.jpg';
+images[3] = 'assets/images/work/Liquidperfection 003.jpg';
+images[4] = 'assets/images/work/Liquidperfection 004.jpg';
+images[5] = 'assets/images/work/Liquidperfection 005.jpg';
+images[6] = 'assets/images/work/Liquidperfection 006.jpg';
+images[7] = 'assets/images/work/Liquidperfection 007.jpg';
+images[8] = 'assets/images/work/Liquidperfection 008.jpg';
+images[9] = 'assets/images/work/Liquidperfection 009.jpg';
+images[10] = 'assets/images/work/Liquidperfection 010.jpg';
+images[11] = 'assets/images/work/Liquidperfection 011.jpg';
+images[12] = 'assets/images/work/Liquidperfection 012.jpg';
+images[13] = 'assets/images/work/Liquidperfection 013.jpg';
+images[14] = 'assets/images/work/Liquidperfection 014.jpg';
+images[15] = 'assets/images/work/Liquidperfection 015.jpg';
+images[16] = 'assets/images/work/Liquidperfection 016.jpg';
+images[17] = 'assets/images/work/Liquidperfection 017.jpg';
+images[18] = 'assets/images/work/Liquidperfection 018.jpg';
+images[19] = 'assets/images/work/Liquidperfection 019.jpg';
+images[20] = 'assets/images/work/Liquidperfection 020.jpg';
 
 function changePicture() {
     document.getElementById("banner").style.backgroundImage = "url(" + images[i] + ")";
