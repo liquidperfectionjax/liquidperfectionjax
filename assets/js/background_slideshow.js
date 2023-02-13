5d8291194773e873100355fc6ea341086c700a1d
Cygnus69 <hilton992003@yahoo.com>
2023-02-13 15:04:30 -0800

    Update background_slideshow.js
    Signed-off-by: Cygnus69 <hilton992003@yahoo.com>

--- assets/js/background_slideshow.js
+++ assets/js/background_slideshow.js
@@ -2,7 +2,7 @@ var i = 0;
 var images = [];
 var slideTime = 3000; // 3 seconds
 
-images[0] = 'assets/images/work/Liquidperfection000.jpg';
+images[0] = 'assets/images/work/Liquidperfection0000.jpg';
 images[1] = 'assets/images/work/Liquidperfection001.jpg';
 images[2] = 'assets/images/work/Liquidperfection002.jpg';
 images[3] = 'assets/images/work/Liquidperfection003.jpg';
@@ -22,7 +22,7 @@ images[16] = 'assets/images/work/Liquidperfection016.jpg';
 images[17] = 'assets/images/work/Liquidperfection017.jpg';
 images[18] = 'assets/images/work/Liquidperfection018.jpg';
 images[19] = 'assets/images/work/Liquidperfection019.jpg';
-images[20] = 'assets/images/work/Liquidperfection020.jpg';
+images[20] = 'assets/images/work/Liquidperfection000.jpg';
 
 function changePicture() {
     document.getElementById("banner").style.backgroundImage = "url(" + images[i] + ")";
